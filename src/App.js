6c7d00c319083a76c88f1384ed6794680753d2bc
Aakash Saini <aakash.saini@chicmic.co.in>
2023-02-21 16:37:48 +0530

    21

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import HooksIntro from './components/HooksIntro';
 import TodoList from './components/TodoList';
 import Task from './components/Task';
 import UseEffect from './components/UseEffect';
+import CompA from './components/CompA';
+import HookuseRef from './components/HookuseRef';
+import HookuseReducer from './components/HookuseReducer';
 function App(){
   return (
  <div>
 
-  <TodoList/>
+  <HookuseReducer/>
 
 
  </div>
